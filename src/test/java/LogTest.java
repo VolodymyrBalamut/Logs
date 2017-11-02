bce7403e4b6710219ca1c9ff7b5280bf74bb3a3f
Volodymyr Balamut <volodymyrbalamut@gmail.com>
2017-11-02 23:39:20 +0200

    final commit

--- src/test/java/LogTest.java
+++ src/test/java/LogTest.java
@@ -1,12 +1,16 @@
 import org.bson.Document;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
+import org.junit.runners.MethodSorters;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import static com.mongodb.client.model.Filters.*;
 import static org.junit.Assert.*;
 
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class LogTest {
     @Test
     public void getIp() throws Exception {
@@ -14,6 +18,7 @@ public class LogTest {
         List<String> list = Log.GetIp("http://www.pravda.com.ua/");
         List<String> test = new ArrayList<>();
         test.add("127.0.0.0");
+        test.add("164.0.0.12");
         assertEquals(list.equals(test), true);
     }
 
@@ -37,32 +42,43 @@ public class LogTest {
 
 
     @Test
-    public void insertDocument(){
+    public void AinsertDocument(){
+       Log.GetCollection();
+       Log log = new Log("http://kkk.kiev.ua/","164.0.0.12","2017/11/02 15:32:00",100);log.InsertDocument();
+       log.InsertDocument();
+       Document doc = Log.logsCollection.find(and(eq("url","http://kkk.kiev.ua/"),eq("ip","164.0.0.12"))).first();
+       assertEquals(doc.isEmpty(),false);
+    }
+
+    @Test
+    public void BupdateDocument(){
         Log.GetCollection();
-        Log log = new Log("www/dynamo","164.0.0.12","2017/11/02 15:32:00",90);
-        log.InsertDocument();
-        Document doc = Log.logsCollection.find(and(eq("url","www/dynamo"),eq("ip","164.0.0.12"))).first();
+        Log log = new Log("http://kkkk.kiev.ua/","164.0.0.12","2017/11/02 15:32:00",90);
+        log.UpdateDocument("http://kkk.kiev.ua/");
+        Document doc = Log.logsCollection.find(and(eq("url","http://kkkk.kiev.ua/"),eq("ip","164.0.0.12"))).first();
         assertEquals(doc.isEmpty(),false);
     }
 
     @Test
-    public void updateDocument(){
+    public void CdeleteDocument() {
         Log.GetCollection();
-        Log log = new Log("http://dynamo.kiev.ua/","164.0.0.12","2017/11/02 15:32:00",90);
-        log.UpdateDocument("www/dynamo");
-        Document doc = Log.logsCollection.find(and(eq("url","http://dynamo.kiev.ua/"),eq("ip","164.0.0.12"))).first();
-        assertEquals(doc.isEmpty(),false);
-    }
-
-    @Test
-    public void deleteDocument() {
-        Log.GetCollection();
-        Log log = new Log("http://kkkk.kiev.ua/","164.0.0.12","2017/11/02 15:32:00",100);
-        log.InsertDocument();
+        Log log = new Log("http://kkkk.kiev.ua/","164.0.0.12","2017/11/02 15:32:00",90);
         log.DeleteDocument();
         Document doc = Log.logsCollection.find(and(eq("url","http://kkkk.kiev.ua/"),eq("ip","164.0.0.12"))).first();
         assertEquals(doc,null);
     }
+
+    //@Test
+    //public void mapReduceURLByTime(){
+       // HashMap<String,String> dict = Log.MapReduceURLByTime();
+        //HashMap<String,String> test = new HashMap<String,String>();
+        //test.put("http://dynamo.kiev.ua/","930.0");
+        //test.put("http://www.pravda.com.ua/","50.0");
+        //test.put("https://docs.mongodb.com/","700.0");
+        //test.put("https://stackoverflow.com/","280.0");
+        //test.put("https://uk.wikipedia.org/" ,"60.0");
+        //assertEquals(dict.equals(test),true);
+    //}
 
 
 }
